84ef0a2ba7277c95c76650215cc303b604e80693
Guillermo <guilleiguaran@gmail.com>
2010-04-14 15:13:17 -0500

    Adding Map in index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   
   def index
     
-    places = session[:places].nil? ? ["Barranquilla"] : session[:places]
+    places = ["Estadio Romelio Martinez, Barranquilla", "Estadio Metropolitano, Barranquilla"]
     results = []
     places.each do |place|
       result = Geocoding::get(place)
@@ -13,12 +13,48 @@ class PagesController < ApplicationController
     
     @map = GMap.new("map_div")
     @map.control_init(:large_map => true, :map_type => true)
-    @map.center_zoom_init(results[0][:latlon],4)
-    @map.clear_overlays
-    results.each do |result|
-      marker = GMarker.new(result[:latlon], :title => result[:address])
-      @map.add_overlay(marker)
-    end
+    @map.center_zoom_init(results[0][:latlon],13)
     
+    @start_marker = GMarker.new(results[0][:latlon], :title => "Inicio", :info_window =>"Arrastre al punto de inicio", :draggable => true)
+    @end_marker   = GMarker.new(results[1][:latlon], :title => "Fin", :info_window =>"Arrastre al punto de final", :draggable => true)
+    
+    @map.declare_init(@start_marker,"start")
+    @map.declare_init(@end_marker,"end")
+    
+    @map.overlay_init(@start_marker)
+    @map.overlay_init(@end_marker)
+    
+    #@map.clear_overlays
+    #results.each do |result|
+    #  marker = GMarker.new(result[:latlon], :title => result[:address])
+    #  @map.add_overlay(marker)
+    #end
+    
+    @map.event_init(@start_marker, :dragend,
+                    "function()
+                    {
+                      var latitud_origen = document.getElementById('latitud_origen');
+                      var longitud_origen = document.getElementById('longitud_origen');
+                      var latitud = this.getLatLng().lat()+'';
+                      var longitud = this.getLatLng().lng()+'';
+                      latitud_origen.value = latitud.substring(0,latitud.indexOf('.')+8);
+                      longitud_origen.value = longitud.substring(0,longitud.indexOf('.')+8);
+                    }
+    ")
+    @map.event_init(@end_marker, :dragend,
+                    "function()
+                    {
+                      var latitud_destino = document.getElementById('latitud_destino');
+                      var longitud_destino = document.getElementById('longitud_destino');
+                      var latitud = this.getLatLng().lat()+'';
+                      var longitud = this.getLatLng().lng()+'';
+                      latitud_destino.value = latitud.substring(0,latitud.indexOf('.')+8);
+                      longitud_destino.value = longitud.substring(0,longitud.indexOf('.')+8);
+                    }
+    ")
+    @start_lat = results[0][:latlon][0]
+    @start_lon = results[0][:latlon][1]
+    @end_lat = results[1][:latlon][0]
+    @end_lon = results[1][:latlon][1]    
   end
 end
